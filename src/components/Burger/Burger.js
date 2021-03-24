01a686ed0b67907c4880406c38a56bb93ac21567
mukundc-iprogrammer <mukundc@iprogrammer.com>
2021-03-24 21:00:00 +0530

    second commit

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-import classes from './Burger.css';
+import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igkey => {
+            return [...Array(props.ingredients[igkey])].map((_, i) => {
+                return <BurgerIngredient key={igkey + i} type={igkey} />;
+            }); 
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+    console.log(transformedIngredients)
     return (
-        <div className={classes.Burger}>
+        <div className='Burger'>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {/* <BurgerIngredient type='cheese' />
+            <BurgerIngredient type='meat' /> */}
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
